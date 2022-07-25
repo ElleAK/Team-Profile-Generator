b586b736fd2a9d5f88fe7646de32ed8e4e380f7b
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-25 12:41:56 -0400

    add engineer and test

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern'); 
-
+const path = require("path");
 const fs = require('fs'); 
 const inquirer = require('inquirer');
 
+teamArray = [];
 
-new Employee().initializeEmployee();
+function startApp () {
+
+    function createTeam () {
+      inquirer.prompt([{
+        type: "list",
+        message: "Job title of the employee you would like to add?",
+        name: "addEmployee",
+        choices: ["Manager", "Engineer", "Intern", "No others needed."]
+      }]).then(function (userInput) {
+        switch(userInput.addEmployee) {
+          case "Manager":
+            addManager();
+            break;
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+  
+          default:
+            htmlBuilder();
+        }
+      })
+    } 
+
+      function addEngineer() {
+          inquirer.prompt([
+            
+            {
+              type: "input",
+              name: "engineerName",
+              message: "What is the engineer's name?"
+            },
+      
+            {
+              type: "input",
+              name: "engineerId",
+              message: "What is the engineer's employee ID number?" 
+            },
+      
+            {
+              type: "input",
+              name: "engineerEmail",
+              message: "What is the engineer's email address?"
+            },
+      
+            {
+              type: "input",
+              name: "engineerGithub",
+              message: "What is the engineer's GitHub username?"
+            }
+      
+          ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamArray.push(engineer);
+            createTeam();
+          });
+      
+        }
+    
+      
+      }
+      
+      startApp();
+
